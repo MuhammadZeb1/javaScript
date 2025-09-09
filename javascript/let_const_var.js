bfbdc1dad0fd6ac30ecb275d1fe9f38f4fbe994e
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-09 10:19:56 -0700

    we learn the let const var and without that

---
+++ javascript/let_const_var.js
@@ -0,0 +1,24 @@
+// console.log("ali")
+
+// let key word
+
+const  accountId =  "2348hhh";
+// change value only
+let accountEmil = "ali@gmail.com"
+// let accountEmil = "jdjjdjd"
+//  change value and re-declier
+var accountPasswaord = "2383474"
+//  
+accountCity = "peshawar"
+
+// accountId ="ee"  not allowed that type
+accountEmil=3434
+
+accountPasswaord="ikdjf"
+
+accountCity="kdfjdfdj"
+
+console.table([accountId,accountEmil,accountPasswaord,accountCity])
+
+
+
