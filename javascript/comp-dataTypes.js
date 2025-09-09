8f082187128a6196c43743382b79bc18dc736daa
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-09 11:39:20 -0700

    we learn copmarsion of the datatypes

---
+++ javascript/comp-dataTypes.js
@@ -0,0 +1,12 @@
+// console.log("3"> 2)
+console.log(null> 0)
+console.log(null>= 0)
+console.log(null== 0)
+console.log(null<= 0)
+
+
+//  Compersion null to convert to a number 
+
+// let nu = null
+
+// console.log(Number(nu)) null = 0
--- javascript/dataTypes.js
+++ javascript/dataTypes.js
@@ -25,3 +25,5 @@
 
 // console.log(typeof str)
 // console.log(typeof null)  type object 
+
+
