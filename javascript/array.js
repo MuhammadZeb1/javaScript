3e635c9125b4adad705025e928a585daff3d7bea
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-23 03:28:04 -0700

    add the react app

---
+++ javascript/array.js
@@ -0,0 +1,11 @@
+const number = [1,2,23,4,4,4,]  // number array and 1 method of the array
+const myArray = new Array("ali","khan",)  // 2 method of the array
+
+console.log(number.length) // 7
+console.log(myArray.length) // 2
+
+number.push(45)// add 45 in the last of the array
+console.log(number)
+
+number.pop()// remove last element of the array
+console.log(number)
