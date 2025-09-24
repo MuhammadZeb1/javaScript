d6aa7546a110e2d1363ee7a2f99a86f6e286a129
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-24 03:00:03 -0700

    learn the use element the without the jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,15 @@
+import { createElement } from "react"
 
-
+createElement
 function App() {
 
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+  // return (
+  //   <>
+  //     <h1>hello</h1>
+  //   </>
+  // )
+
+  return createElement("div",{id:"my"}, "hello my")
 }
 
 export default App
