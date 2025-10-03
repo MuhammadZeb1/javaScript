9ac51a8ad0cc5fb3f294937f4b8fb9278c48f224
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-03 11:06:03 -0700

    learn about the function

---
+++ javascript/function.js
@@ -0,0 +1,30 @@
+
+// function declaration
+function fun (){
+console.log("ali")
+}
+
+// fun()// call the function.
+
+// function addTwoNums(num1,num2){
+//     console.log(num1 + num2)
+// }
+//  const sum = addTwoNums(2,3)//5
+//  console.log(sum)//  undefined(it is not return any value)
+
+// function addTwoNums(num1,num2){
+//     return num1 + num2
+// }
+//  const sum = addTwoNums(2,3)//5
+
+//  console.log(sum)//  undefined(it is not return any value)
+
+
+function strFun(username) {
+    return `hello ${username}`;
+}
+
+const result = strFun("ali");
+
+// Output دکھانے کے لیے
+console.log(result);
