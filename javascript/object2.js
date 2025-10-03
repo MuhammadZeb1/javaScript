54c503d7458b653b04e8aff57561ad944d69fae6
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-03 07:04:55 -0700

    learn the object destruture

--- javascript/object2.js
+++ javascript/object2.js
@@ -2,13 +2,13 @@
 
 // console.log("obje",obj)
 
-const obj = {}// object letural 
+// const obj = {}// object letural 
 
-obj.id="33334"
-obj.name="muhammad zeb "
-obj.greeting=function(){
-    console.log(`hello ${this.name}`)
-}
+// obj.id="33334"
+// obj.name="muhammad zeb "
+// obj.greeting=function(){
+//     console.log(`hello ${this.name}`)
+// }
 // console.log("obj",obj.greeting())
 
 // const nestObj = {
@@ -22,9 +22,8 @@ obj.greeting=function(){
 // }
 
 // console.log("nestObj",nestObj.fullName.userName.firstName)
-
-const obj2 = {1: "one", 2: "two", 3: "three",}
-const obj3 ={4:"four",5:"five"}
+// const obj2 = {1: "one", 2: "two", 3: "three",}
+// const obj3 ={4:"four",5:"five"}
 
 // const newObj = Object.assign({},obj2,obj3)// merge two object
 // const newObj = Object.assign(obj2,obj3)// merge two object
@@ -50,10 +49,21 @@ const obj3 ={4:"four",5:"five"}
 // ]
 // console.log("arrayOfObj",arrayOfObj[1].name)
   
-console.log("obj",Object.keys(obj))// return array of keys
-console.log("obj",Object.values(obj))// return array of values
-console.log("obj",Object.entries(obj))// return array of array key value pair
-console.log("obj",Object.hasOwn(obj,"id"))// return boolean if key exist or not
-console.log("obj",Object.hasOwnProperty(obj,"age"))// return boolean if key exist or not
+// console.log("obj",Object.keys(obj))// return array of keys
+// console.log("obj",Object.values(obj))// return array of values
+// console.log("obj",Object.entries(obj))// return array of array key value pair
+// console.log("obj",Object.hasOwn(obj,"id"))// return boolean if key exist or not
+// console.log("obj",Object.hasOwnProperty(obj,"age"))// return boolean if key exist or not
+
+const  obj = {
+    name :"muhammad zeb ",
+    rollNo :33
+}
+// console.log(obj.name)// muhammad zeb
+
+const {name:jk}=obj// object destruture
+// const {name}=obj// object destruture
+console.log(jk)// muhammad zeb
+
 
 
