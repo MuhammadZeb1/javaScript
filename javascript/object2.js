90ae62bd4caae94f7b110bdf6d88567522b5740e
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-03 06:38:26 -0700

    learn the merge two object

--- javascript/object2.js
+++ javascript/object2.js
@@ -11,14 +11,24 @@ obj.greeting=function(){
 }
 // console.log("obj",obj.greeting())
 
-const nestObj = {
-    id:"3434",
-    fullName :{
-        userName:{
-            firstName:"muhammad",
-            lastName:"zeb"
-        }
-    }
-}
+// const nestObj = {
+//     id:"3434",
+//     fullName :{
+//         userName:{
+//             firstName:"muhammad",
+//             lastName:"zeb"
+//         }
+//     }
+// }
 
-console.log("nestObj",nestObj.fullName.userName.firstName)
+// console.log("nestObj",nestObj.fullName.userName.firstName)
+
+const obj2 = {1: "one", 2: "two", 3: "three",}
+const obj3 ={4:"four",5:"five"}
+
+// const newObj = Object.assign({},obj2,obj3)// merge two object
+// const newObj = Object.assign(obj2,obj3)// merge two object
+
+const newObj = {...obj2,...obj3} // spread operator
+
+console.log("newObj",newObj)
