ebe5cfdded13bf881d5bbccf7495bcbd4e18cdff
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-04 06:45:41 -0700

    learn the more the about the scope

--- javascript/scope.js
+++ javascript/scope.js
@@ -14,4 +14,37 @@ var c = 11;
 
 // console.log(a);
 // console.log(b);
-console.log(c);// output 11 
+// console.log(c);// output 11 
+
+// function testFunction() {
+//   let localVar = "I am local";
+//   console.log(localVar); // ✅ Accessible
+// }
+
+// console.log(localVar); // ❌ Error
+
+// testFunction();
+
+// local scope : are the scope inside the function 
+// global scope : are the scope outside the function
+// block scope : are the scope inside the block
+
+
+// nested function 
+
+function one (){
+    const name = "ali"
+
+    function two (){
+
+        const lName = "khan"
+        
+        console.log(name)
+    }
+        console.log(lName)
+
+        two()
+
+}
+
+one()
