7044ba08b5e945b3aed18f5dfa247301ce18ffd4
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-04 00:28:23 -0700

    learn the block scope local scope and global scope

---
+++ javascript/scope.js
@@ -0,0 +1,17 @@
+// scope in js 
+
+// {} block scope 
+
+// let a = 9;
+// const b = 10;
+// var c = 11;
+
+if (true) {
+    let a = 9;
+const b = 10;
+var c = 11;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);// output 11 
