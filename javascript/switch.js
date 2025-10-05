484d7f758437253214f9507b214bf03d982a01b5
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-05 10:50:16 -0700

    learn about the falsy and truthy values and nullish and ternary operator

--- javascript/switch.js
+++ javascript/switch.js
@@ -41,3 +41,60 @@
 Output:
 Good
 */
+
+
+
+
+// falsy value
+// false,0 (zero).-0 (negative zero),0n (BigInt zero),"" (empty string),null,undefined
+
+
+// truthy value
+// true,{},[],1,"hello",3n,"false"
+
+
+// check the empty array or object
+// if (arr.length === 0) {
+//     console.log("Array is empty");
+//   }
+
+//   if (Object.keys(obj).length === 0) {
+//     console.log("Object is empty");
+//   }
+
+
+// nullish coalescing operator\
+
+// let val1;
+// console.log(val1 ?? "Default value"); // Default value
+
+// let val2 = null;
+// console.log(val2 ?? "Default value"); // Default value
+
+// nullish-coalescing.js
+
+// let username = null;
+// let displayName = username ?? "Guest";
+
+// console.log(displayName);
+
+/*
+Output:
+Guest
+*/
+
+// ternery operator
+// condition ? true : false
+// ternary-operator.js
+
+// let age = 20;
+// let result = (age >= 18) ? "You are an adult." : "You are a minor.";
+
+// console.log(result);
+
+/*
+Output:
+You are an adult.
+*/
+
+
