c1d568c654c9cf212f774bfdff6c724505eb200a
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-05 05:26:26 -0700

    dicuss the this in the function

--- javascript/arrow.js
+++ javascript/arrow.js
@@ -1,16 +1,31 @@
 // this keyword refer the current context of the object
-const obj = {
-    name :"muhammad zeb ",
-      rollNo :33,
-      greeting : function(){
-          console.log(`hello ${this.name}`)
-          console.log(this)// show the current context
-        }
-    }
+// const obj = {
+//     name :"muhammad zeb ",
+//       rollNo :33,
+//       greeting : function(){
+//           console.log(`hello ${this.name}`)
+//           console.log(this)// show the current context
+//         }
+//     }
 
     // obj.greeting()
     // obj.name = "ali"
     // obj.greeting()
     // this refer the current context of the function
 
-    console.log(this)// in node refer the empty object {} (and browser refer the window  object )
+    // console.log(this)// in node refer the empty object {} (and browser refer the window  object )
+
+
+    // function chai (){
+    //   const username = "ali"
+    //   console.log(this.username)//undefined(work only the object)
+    // }
+
+    // chai()
+
+     const chai= ()=>{
+      const username = "ali"
+      console.log(this) //{}
+    }
+
+    chai()
