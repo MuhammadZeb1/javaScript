457754d16cea08a8cf833be52423a8dd13084ce6
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-05 10:19:54 -0700

    leran the if else

---
+++ javascript/if_else_examples.js
@@ -0,0 +1,47 @@
+// if-else-examples.js
+
+// Example 1: if statement
+let age1 = 20;
+
+if (age1 >= 18) {
+  console.log("You are eligible to vote.");
+}
+
+/*
+Output:
+You are eligible to vote.
+*/
+
+
+// Example 2: if...else statement
+let age2 = 16;
+
+if (age2 >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
+
+/*
+Output:
+You are not eligible to vote.
+*/
+
+
+// Example 3: if...else if...else statement
+let marks = 75;
+
+if (marks >= 90) {
+  console.log("Grade: A+");
+} else if (marks >= 80) {
+  console.log("Grade: A");
+} else if (marks >= 70) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: C");
+}
+
+/*
+Output:
+Grade: B
+*/
