52620751d99131f336706fa9041c5efc15982126
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-07 16:24:07 +0500

    for in loop on object

---
+++ javascript/for-in-loop.js
@@ -0,0 +1,17 @@
+const obj = {
+    name :"muhammad",
+    age : 22,
+    rollNo : 33
+}
+
+for (const key in obj) {
+    console.log(key,obj[key]);// key and value
+}
+
+
+// on array 
+
+const arr = [1, 2, 3, 4, 5];
+for (const key in arr) {
+    // console.log(key, arr[key]);// key and value
+}
