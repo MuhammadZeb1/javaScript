7877172a164f75ef947a54f1e2838407029a8336
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-14 08:12:37 +0500

    dicuess the map loop

--- javascript/loop-3.js
+++ javascript/loop-3.js
@@ -8,5 +8,9 @@ const arr = ["ali","khan","numan","ahmed"];
 
 const num = [1,2,3,4,5,6,7,8,9,10];
 
- const result = num.filter((num)=> (num >5))
- console.log(result)
+//  const result = num.filter((num)=> (num >5))
+//  console.log(result)
+
+const result = num.map((num)=> (num *2))// return boolean value
+// const result = num.map((num)=> (num > 2))// return boolean value
+console.log(result)
