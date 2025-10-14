4b27c016e904ce65a246a7eec499b98a09c7ff38
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-14 08:03:42 +0500

    dicuess the filter loop

--- javascript/loop-3.js
+++ javascript/loop-3.js
@@ -5,3 +5,8 @@ const arr = ["ali","khan","numan","ahmed"];
 // });
 
 // console.log(result)
+
+const num = [1,2,3,4,5,6,7,8,9,10];
+
+ const result = num.filter((num)=> (num >5))
+ console.log(result)
