46defd457e93b4ab916f2147398c539d5df6de40
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-10-31 18:06:27 +0500

    update the promise file

--- javascript/promise/promise.js
+++ javascript/promise/promise.js
@@ -1,49 +1,103 @@
-// Define the promises
-const promiseOne = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("Task One");
-    resolve("Promise One Resolved");
-  }, 1000);
+
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is compelete');
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
 });
 
-const promiseTwo = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("Task Two");
-    resolve("Promise Two Resolved");
-  }, 1000);
-});
-
-const promiseThree = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("Task Three");
-    resolve("Promise Three Resolved");
-  }, 1000);
-});
-
-const promiseFour = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("Task Four");
-    resolve("Promise Four Resolved");
-  }, 1000);
-});
-
-const promiseFive = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("Task Five");
-    resolve("Promise Five Resolved");
-  }, 1000);
-});
-
-// Handle promises using async/await
-async function handlePromises() {
-  try {
-    
-
-    const result5 = await promiseFive;
-    console.log(result5);
-  } catch (error) {
-    console.log("Error:", error);
-  }
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-handlePromises();
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
